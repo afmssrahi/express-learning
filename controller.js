895d456de7987fef2214b13a329099ceefff093f
afmssrahi <afmssrahi@gmail.com>
2022-03-22 02:27:45 +0600

    know about controller

---
+++ controller.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+exports.homeController = (req, res) => {
+	fs.readFile('./pages/index.html', (err, data) => {
+		if (err) {
+			console.log('Error', err);
+			res.send('<h1>Something went wrong.</h1>');
+		} else {
+			res.write(data);
+			res.end();
+		}
+	});
+};
+
+exports.aboutController = (req, res) => {
+	fs.readFile('./pages/about.html', (err, data) => {
+		if (err) {
+			console.log('Error', err);
+			res.send('<h1>Something went wrong.</h1>');
+		} else {
+			res.write(data);
+			res.end();
+		}
+	});
+};
+
+exports.productsController = (req, res) => {
+	fs.readFile('./pages/products.html', (err, data) => {
+		if (err) {
+			console.log('Error', err);
+			res.send('<h1>Something went wrong.</h1>');
+		} else {
+			res.write(data);
+			res.end();
+		}
+	});
+};
--- routes.js
+++ routes.js
@@ -1,40 +1,14 @@
 const router = require('express').Router();
-const fs = require('fs');
+const {
+	homeController,
+	aboutController,
+	productsController,
+} = require('./controller');
 
-router.get('/', (req, res) => {
-	fs.readFile('./pages/index.html', (err, data) => {
-		if (err) {
-			console.log('Error', err);
-			res.send('<h1>Something went wrong.</h1>');
-		} else {
-			res.write(data);
-			res.end();
-		}
-	});
-});
+router.get('/', homeController);
 
-router.get('/about', async (req, res) => {
-	fs.readFile('./pages/about.html', (err, data) => {
-		if (err) {
-			console.log('Error', err);
-			res.send('<h1>Something went wrong.</h1>');
-		} else {
-			res.write(data);
-			res.end();
-		}
-	});
-});
+router.get('/about', aboutController);
 
-router.get('/products', (req, res) => {
-	fs.readFile('./pages/products.html', (err, data) => {
-		if (err) {
-			console.log('Error', err);
-			res.send('<h1>Something went wrong.</h1>');
-		} else {
-			res.write(data);
-			res.end();
-		}
-	});
-});
+router.get('/products', productsController);
 
 module.exports = router;
