359a26bc15a3a5435423e5a64e456c6123b635db
afmssrahi <afmssrahi@gmail.com>
2022-03-22 01:47:37 +0600

    know about global and local middleware. and create  custom global and custom local middleware for example.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(morgan('dev'));
 app.use(cors());
+app.use(globalMiddleware);
 
 app.get('/', (req, res) => {
 	fs.readFile('./pages/index.html', (err, data) => {
@@ -23,7 +24,7 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.get('/about', morgan('dev'), async (req, res) => {
+app.get('/about', [morgan('dev'), localMiddleware], async (req, res) => {
 	fs.readFile('./pages/about.html', (err, data) => {
 		if (err) {
 			console.log('Error', err);
@@ -50,3 +51,26 @@ app.get('/products', (req, res) => {
 app.listen(port, () => {
 	console.log(`Server is listening on PORT ${port}`);
 });
+
+// middlewareSignature
+function middlewareSignature(req, res, next) {
+	next();
+}
+
+// custom global middleware
+function globalMiddleware(req, res, next) {
+	console.log(`${req.method} - ${req.url}`);
+	console.log('I am a global middleware');
+
+	if (req.query.bad) {
+		return res.status(400).send('Bad Request');
+	}
+
+	next();
+}
+
+// custom local middleware
+function localMiddleware(req, res, next) {
+	console.log('I am Local middleware');
+	next();
+}
