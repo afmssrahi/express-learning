0465a28398f451380023c0574ffd082f97ce710e
afmssrahi <afmssrahi@gmail.com>
2022-03-24 18:46:43 +0600

    render dynamic data in ejs

--- controller.js
+++ controller.js
@@ -1,14 +1,20 @@
 exports.homeController = (req, res) => {
 	// throw new Error('Something went wrong');
-	const title = 'Dynamic title (Home)';
+	const title = 'Home page';
+	const activeHome = true;
 
 	res.render('pages/home', { title });
 };
 
 exports.aboutController = (req, res) => {
-	res.render('pages/about');
+	const title = 'About page';
+	const activeAbout = true;
+
+	res.render('pages/about', { title });
 };
 
 exports.productsController = (req, res) => {
-	res.render('pages/products');
+	const title = 'Product page';
+
+	res.render('pages/products', { title });
 };
